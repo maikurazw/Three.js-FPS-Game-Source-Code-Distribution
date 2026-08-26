300a1480254170c235e35a27c4630a71c3eb7484
maikurazw <maikurazw@proton.me>
2026-08-26 20:22:59 +0900

    Update three.core.js

--- build/three.core.js
+++ build/three.core.js
@@ -1,1 +1,7 @@
-
+// Three.jsが内部で探している部品を、エラーが起きないように空っぽの状態で用意します
+export const ACESFilmicToneMapping = 0;
+export const CineonToneMapping = 0;
+export const CustomToneMapping = 0;
+export const LinearToneMapping = 0;
+export const NoToneMapping = 0;
+export const ReinhardToneMapping = 0;
