02e67f8e96f6e7a045170f20792cff08202fbd7f
maikurazw <maikurazw@proton.me>
2026-08-26 18:49:31 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-// 🌍 1. インターネット上の公式ファイルを直接インポートする（URLの末尾まで全て必要です）
-import * as THREE from ''three';';
+// 🌍 1. インターネット上の公式ファイルを直接インポート（URLの最後まで全て必要です）
+import * as THREE from 'https://cloudflare.com';
 
 // 2. シーン（世界）を作成
 const scene = new THREE.Scene();
